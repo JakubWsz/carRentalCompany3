77860267436aa724e80bd6dfaee90356c07bde81
jakubwsz <jakub.wszola96@gmail.com>
2022-01-09 19:41:26 +0100

    initial commit

---
+++ src/main/java/com/kuba/carrentalcompany3/CarRentalCompany3Application.java
@@ -0,0 +1,13 @@
+package com.kuba.carrentalcompany3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalCompany3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalCompany3Application.class, args);
+    }
+
+}
