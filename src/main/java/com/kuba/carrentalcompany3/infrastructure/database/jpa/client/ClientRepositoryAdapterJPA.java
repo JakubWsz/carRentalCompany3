7a34cdabea57cda0fb73292b28330cd079b66ef6
jakubwsz <jakub.wszola96@gmail.com>
2022-01-31 22:26:08 +0100

    config changes

--- src/main/java/com/kuba/carrentalcompany3/infrastructure/database/jpa/client/ClientRepositoryAdapterJPA.java
+++ src/main/java/com/kuba/carrentalcompany3/infrastructure/database/jpa/client/ClientRepositoryAdapterJPA.java
@@ -4,9 +4,8 @@ import com.kuba.carrentalcompany3.domain.client.ClientRepository;
 import com.kuba.carrentalcompany3.domain.client.model.Client;
 import com.kuba.carrentalcompany3.infrastructure.database.jpa.client.entity.ClientDao;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public class ClientRepositoryAdapterJPA implements ClientRepository {
     private final ClientRepositoryJPA clientRepositoryJPA;
     private final ConversionService conversionService;
